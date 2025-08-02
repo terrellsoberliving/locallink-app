442c7a17e6c2a0d65eef2f51b72627e4a5a1344d
David Soule <terrellsoberliving@gmail.com>
2025-08-02 10:58:31 -0500

    Initial commit of working LocalLink starter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to LocalLink</h1>
+      <p>This is your starter app for connecting service providers and clients.</p>
+    </div>
+  );
+}
+
+export default App;
